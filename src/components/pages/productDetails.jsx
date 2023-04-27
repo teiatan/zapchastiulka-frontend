288842fe42dbc8e0395345d8f3dee3f9b1de9f32
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-27 23:37:04 +0300

    add request on product details page

--- src/components/pages/productDetails.jsx
+++ src/components/pages/productDetails.jsx
@@ -1,5 +1,25 @@
+import { apiService } from "api/apiService";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
 export function ProductDetails() {
+    const [productData, setProductData] = useState({});
+    const { productId } = useParams();
+
+    useEffect(()=>{
+        apiService.getproductById(productId).then(
+          response => setProductData(response)
+        );
+    }, [productId]);
+
+    console.log(productData);
+
     return(
-        <p>Product details page</p>
+        <>
+            <p>Product details page</p>
+            <p>{productData.name}</p>
+        </>
+        
     )
 };
