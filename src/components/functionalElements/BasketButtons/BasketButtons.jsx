4e9f66131d3825a4a27dbd1c3c0d001549cd96de
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-07 13:25:13 +0300

    make clear basket buttons code

--- src/components/functionalElements/BasketButtons/BasketButtons.jsx
+++ src/components/functionalElements/BasketButtons/BasketButtons.jsx
@@ -4,19 +4,11 @@ export function BasketButtons({productId}) {
 
     const basket = useBasket();
 
-    const addToBasket = () => {
-        basket.addToBasket(productId);
-    };
-
-    const removeFromBasket = () => {
-        basket.removeFromBasket(productId);
-    };
-
     return (
         <>
         {basket.isInBasket(productId) ?
-        <button onClick={removeFromBasket}>Remove from basket</button>:
-        <button onClick={addToBasket}>Add to basket</button>
+        <button onClick={() => basket.removeFromBasket(productId)}>Remove from basket</button>:
+        <button onClick={() => basket.addToBasket(productId)}>Add to basket</button>
         }
         </>    
     )
