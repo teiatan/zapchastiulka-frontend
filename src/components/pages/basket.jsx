9671d4eb587fa127328d24bdca9f737fd7a94932
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-08 23:11:22 +0300

    display goods in basket

--- src/components/pages/basket.jsx
+++ src/components/pages/basket.jsx
@@ -1,13 +1,30 @@
+import { ProductsList } from "components/Main/ProductsList/ProductsList";
 import { useBasket } from "context/contectxtHooks"
+import { useEffect, useState } from "react";
+import {apiService} from "../../api/apiService";
 
 export function Basket() {
 
     const basket = useBasket();
+    const [shownGoods, setshownGoods] = useState([]);
+    
+    useEffect(()=> {
+        basket.basket.map(id => {
+            if(shownGoods.findIndex(product => product._id === id) === -1) {
+                apiService.getproductById(id).then(
+                    response => {
+                        setshownGoods([...shownGoods, response]);
+                    }
+                );    
+            };
+        });
+        
+    }, [basket.basket, shownGoods]);
 
     return (
         <>
             <h1>Корзина товарів</h1>
-            <p>ID товарів: {basket.basket}</p>
+            <ProductsList shownGoods={shownGoods}/>
         </>
         
     )
