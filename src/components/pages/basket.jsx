ea24482aaa4209c42637e214ac0438f62a96bbfc
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-09 00:01:03 +0300

    Update basket.jsx

--- src/components/pages/basket.jsx
+++ src/components/pages/basket.jsx
@@ -1,30 +1,43 @@
 import { ProductsList } from "components/Main/ProductsList/ProductsList";
 import { useBasket } from "context/contectxtHooks"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {apiService} from "../../api/apiService";
 
 export function Basket() {
 
     const basket = useBasket();
     const [shownGoods, setshownGoods] = useState([]);
-    
+    const sumPrice = useRef(0);
+
     useEffect(()=> {
-        basket.basket.map(id => {
+        /* basket.basket.map(id => {
             if(shownGoods.findIndex(product => product._id === id) === -1) {
                 apiService.getproductById(id).then(
                     response => {
+                        console.log('get');
+                        setshownGoods([...shownGoods, response]);
+                    }
+                );    
+            };
+        }); */
+        basket.basket.forEach(id => {
+            if(shownGoods.findIndex(product => product._id === id) === -1) {
+                apiService.getproductById(id).then(
+                    response => {
+                        console.log('get');
                         setshownGoods([...shownGoods, response]);
                     }
                 );    
             };
         });
-        
+        sumPrice.current = shownGoods.reduce((sum, product) => sum + product.price, 0);
     }, [basket.basket, shownGoods]);
 
     return (
         <>
             <h1>Корзина товарів</h1>
             <ProductsList shownGoods={shownGoods}/>
+            <p>Сума замовлення: {sumPrice.current}</p>
         </>
         
     )
